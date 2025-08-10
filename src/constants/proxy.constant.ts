374662cf61b5dba5105280461a94da0cb61ad96d
Adi Wahyudi <adi.whyd02@gmail.com>
2025-08-10 17:00:10 +0700

    feat: add proxies

---
+++ src/constants/proxy.constant.ts
@@ -0,0 +1,12 @@
+export const PROXIES = [
+  'http://ofpxirls:zp7xbtj240lm@23.95.150.145:6114',
+  'http://ofpxirls:zp7xbtj240lm@198.23.239.134:6540',
+  'http://ofpxirls:zp7xbtj240lm@45.38.107.97:6014',
+  'http://ofpxirls:zp7xbtj240lm@207.244.217.165:6712',
+  'http://ofpxirls:zp7xbtj240lm@107.172.163.27:6543',
+  'http://ofpxirls:zp7xbtj240lm@104.222.161.211:6343',
+  'http://ofpxirls:zp7xbtj240lm@64.137.96.74:6641',
+  'http://ofpxirls:zp7xbtj240lm@216.10.27.159:6837',
+  'http://ofpxirls:zp7xbtj240lm@136.0.207.84:6661',
+  'http://ofpxirls:zp7xbtj240lm@142.147.128.93:6593',
+];
